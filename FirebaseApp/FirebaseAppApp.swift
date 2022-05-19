7d0ae2f6c60e7aa00b2dab1f9e622fbea3bd8356
palmachris7 <palmachristopher7@hotmail.com>
2022-05-18 20:04:35 -0500

    Initial Commit

---
+++ FirebaseApp/FirebaseAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirebaseAppApp.swift
+//  FirebaseApp
+//
+//  Created by Christopher Palma Talaverano on 18/05/22.
+//
+
+import SwiftUI
+
+@main
+struct FirebaseAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
